9a8912feff11eed475605e094cb214ef9e4db3ab
Carla Antunes pereira <kamalleoah@gmail.com>
2025-09-27 00:41:43 +0000

    iniciar comentarios

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,10 +6,81 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
+    //Carta 1
+
+    char estado1;
+    char codigo1[4];
+    char nomeCidade1[50]; 
+    int populacao1; 
+    float area1; 
+    float pib1; 
+    int pontos_turisticos1; 
+
+     //Carta 2
+
+    char estado2;
+    char codigo2[4];
+    char nomeCidade2[50]; 
+    int populacao2; 
+    float area2; 
+    float pib2; 
+    int pontos_turisticos2;    
 
   // Área para entrada de dados
 
+// Carta 1 
+
+printf("Carta 1: \n");
+
+printf("Estado (escolha  uma letra de A a H): "); 
+scanf("%c", &estado1); 
+
+printf("Codigo (a letra do estado seguida de um numero de 01 a 04): "); 
+scanf("%s", &codigo1);
+
+printf("Nome da cidade: ");
+scanf("%s", &nomeCidade1);
+
+printf("População: ");
+scanf("%d", &populacao1);
+
+printf("Area: ");
+scanf("%f", &area1);
+
+printf("PIB: ");
+scanf("%f", &pib1);
+
+printf("Numero de Pontos Turisticos: ");
+scanf("%d", &pontos_turisticos1);
+
+// Carta 2 
+
+printf("Carta 2: \n");
+
+printf("Estado (escolha  uma letra de A a H): "); 
+scanf("%c", &estado2); 
+
+printf("Codigo (a letra do estado seguida de um numero de 01 a 04): "); 
+scanf("%s", &codigo2);
+
+printf("Nome da cidade: ");
+scanf("%s", &nomeCidade2);
+
+printf("População: ");
+scanf("%d", &populacao2);
+
+printf("Area: ");
+scanf("%f", &area2);
+
+printf("PIB: ");
+scanf("%f", &pib2);
+
+printf("Numero de Pontos Turisticos: ");
+scanf("%d", &pontos_turisticos2);
+
   // Área para exibição dos dados da cidade
+
+
 
 return 0;
 } 
